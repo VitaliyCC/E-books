50258083b6f3da7b8a70f704f3a79e80920fbfc8
VitaliyCC <sawostjan@gmail.com>
2023-09-26 17:22:36 +0300

    Initial Commit

---
+++ src/test/java/com/savostian/ebooks/EBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savostian.ebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
